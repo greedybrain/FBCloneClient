914a0d88022eed2fe58726d6c20f806ffbcf2b46
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-16 07:13:25 -0400

    first commit

---
+++ src/Pages/EntryPage/containers/EntryPageContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from './Header'
+
+export default class EntryPageContainer extends Component {
+    render() {
+        return (
+            <div className="wrapper">
+                <Header />
+            </div>
+        )
+    }
+}
---
+++ src/Pages/EntryPage/containers/NewAccountContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewAccountContainer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default NewAccountContainer
